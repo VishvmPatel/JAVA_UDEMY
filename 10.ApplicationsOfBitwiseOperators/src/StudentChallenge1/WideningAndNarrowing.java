2ec87a0ecc940f98be4da3f6f3056422b41fa721
Vishvam <vishusp77@gmail.com>
2024-10-15 22:37:45 +0530

    update

---
+++ 10.ApplicationsOfBitwiseOperators/src/StudentChallenge1/WideningAndNarrowing.java
@@ -0,0 +1,23 @@
+package StudentChallenge1;
+
+public class WideningAndNarrowing {
+    public static void main(String[] args) {
+        byte b=5;
+        short s=120;
+        int i=10;
+        long l=10;
+        float f=10;
+        double d=10;
+        char c=10;
+        boolean bl=true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bl);
+    }
+}
