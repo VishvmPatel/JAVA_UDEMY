e218575017b8446587cdce8e39378c432f5b4fdb
Vishvam <vishusp77@gmail.com>
2024-10-15 23:05:04 +0530

    update

---
+++ 11.TypesOfPrintStatements/src/Printing.java
@@ -0,0 +1,14 @@
+public class Printing {
+    public static void main(String[] args) {
+
+        int a = 10;
+        float b = 12.55f;
+        char c = 'A';
+        String str = "Hello";
+
+        System.out.println(a);      //Here System.out.println is an overloaded method
+        System.out.println(b);      //As it is able to take different types of parameters.
+        System.out.println(c);      //And able to print them successfully
+        System.out.println(str);
+    }
+}
