c38db665271ca9cd3e496f0bc07c56f683ed01a1
Vishvam <vishusp77@gmail.com>
2024-10-16 23:53:58 +0530

    update

---
+++ 11.TypesOfPrintStatements/src/PrintingAnExpression.java
@@ -0,0 +1,10 @@
+public class PrintingAnExpression {
+    public static void main(String[] args) {
+        int x=10,y=20;
+        System.out.println(x+y);
+        System.out.println("Number is " +y);
+        System.out.println(x+y+" sum");
+        System.out.println("sum "+(x+y));
+        System.out.println("sum of "+x+" and "+y+" is "+(x+y));
+    }
+}
