3840a8f1e5a61fc55c4ce1de60d744ac123d60a6
Vishvam <vishusp77@gmail.com>
2024-10-18 17:48:08 +0530

    update

---
+++ 11.TypesOfPrintStatements/src/Printf.java
@@ -0,0 +1,8 @@
+public class Printf {
+    public static void main(String[] args) {
+        int x= 10;
+        float y = 12.56f;
+        char z = 'A';
+        System.out.printf("Hello %d %f %c \n",x,y,z);
+    }
+}
