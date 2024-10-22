97473b4b1f8459b3743678cfbd357bcb87c255df
Vishvam <vishusp77@gmail.com>
2024-10-22 17:46:17 +0530

    update

---
+++ 15.ConditionalStatements/src/Example1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+//TO CHECK WHETHER THE GIVEN NUMBER IS POSITIVE NEGATIVE OR ZERO.
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Number: ");
+        int x = sc.nextInt();
+        if(x>0){
+            System.out.println("The provided number is Positive");
+        }
+        else if (x==0){
+            System.out.println("The provided number is Zero");
+        }
+        else {
+            System.out.println("The provided number is Negative");
+        }
+    }
+}
