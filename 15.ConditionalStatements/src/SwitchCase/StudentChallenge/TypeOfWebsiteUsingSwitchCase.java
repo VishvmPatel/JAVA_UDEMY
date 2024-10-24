c369277847800bd722463976b06c51cdac7f9333
Vishvam <vishusp77@gmail.com>
2024-10-24 22:58:17 +0530

    update

---
+++ 15.ConditionalStatements/src/SwitchCase/StudentChallenge/TypeOfWebsiteUsingSwitchCase.java
@@ -0,0 +1,25 @@
+package SwitchCase.StudentChallenge;
+
+import java.util.Scanner;
+
+public class TypeOfWebsiteUsingSwitchCase {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter Domain name");
+        String domain=scan.nextLine();
+        String ext=domain.substring(domain.lastIndexOf(".")+1);
+        switch(ext)
+        {
+            case "com": System.out.println("Commercial");
+                break;
+            case "org": System.out.println("Organisation");
+                break;
+
+            case "gov": System.out.println("Government");
+                break;
+            case "net": System.out.println("Network");
+                break;
+        }
+    }
+}
