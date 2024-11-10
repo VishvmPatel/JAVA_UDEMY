98627faa8da655077bdd326cbf4deb3c9d302990
Vishvam <vishusp77@gmail.com>
2024-11-11 01:07:03 +0530

    update

---
+++ 16.Loops/src/StudentChallenge2/CountDigits.java
@@ -0,0 +1,15 @@
+package StudentChallenge2;
+import java.util.Scanner;
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n=scan.nextInt();
+        int count=0;
+        while(n>0) {
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
