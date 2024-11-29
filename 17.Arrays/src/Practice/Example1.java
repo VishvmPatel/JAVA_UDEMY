f6fb453a3fcd9fa745d9b0a9730c73ab60b08e42
Vishvam <vishusp77@gmail.com>
2024-11-29 22:05:29 +0530

    update

---
+++ 17.Arrays/src/Practice/Example1.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int A[] = new int[10];
+        for(int i=0;i<A.length;i++){
+            System.out.println(A[i]);
+        }
+    }
+}
