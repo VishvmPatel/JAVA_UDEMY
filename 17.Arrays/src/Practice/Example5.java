91733ddb9349638e60a891a79e06602c7f5adf05
Vishvam <vishusp77@gmail.com>
2024-11-29 22:15:12 +0530

    update

---
+++ 17.Arrays/src/Practice/Example5.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Example5 {
+    public static void main(String[] args) {
+        int B[] = {2,4,6,8,10};
+        B[2] = 16; //This shows how to change the value of an element in an Array
+        for(int x : B){
+            System.out.println(x);
+        }
+    }
+}
