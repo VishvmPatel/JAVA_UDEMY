111818cf2baa7ac848f7401ea343d15edafbe833
Vishvam <vishusp77@gmail.com>
2024-11-29 22:11:55 +0530

    update

---
+++ 17.Arrays/src/Practice/Example4.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Example4 {
+    public static void main(String[] args) {
+        int B[]= {2,4,6,8,10};
+        for(int x :B){
+            System.out.println(x++);
+        }
+    }
+}
