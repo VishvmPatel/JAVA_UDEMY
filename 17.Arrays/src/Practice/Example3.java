e9c1dca1fa99bd397e26fef7f16aab265dc45e95
Vishvam <vishusp77@gmail.com>
2024-11-29 22:09:18 +0530

    update

---
+++ 17.Arrays/src/Practice/Example3.java
@@ -0,0 +1,12 @@
+package Practice;
+
+//PRINTING USING FOR EACH LOOP
+
+public class Example3 {
+    public static void main(String[] args) {
+        int B[] = {2,4,6,8,10};
+        for(int x:B){
+            System.out.println(x);
+        }
+    }
+}
