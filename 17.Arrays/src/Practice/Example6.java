2ae4e2ae8d3ff4e0b697a39867069e1a9fe173b8
Vishvam <vishusp77@gmail.com>
2024-11-29 22:24:07 +0530

    update

---
+++ 17.Arrays/src/Practice/Example6.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Example6 {
+    public static void main(String[] args) {
+        int B[] = {2,4,6,8,10};
+        for(int i = 0; i<B.length ;i++){
+            System.out.println(B[i]++);
+        }
+        for (int x : B){
+            System.out.println(x);
+        }
+    }
+}
