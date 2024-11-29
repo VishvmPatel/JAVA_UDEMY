6f58b7c29e209c8b02763d43a8bad74a8be24010
Vishvam <vishusp77@gmail.com>
2024-11-29 22:33:43 +0530

    update

---
+++ 17.Arrays/src/StudentChallenge1/FindingTheSumOfElements.java
@@ -0,0 +1,15 @@
+package StudentChallenge1;
+
+public class FindingTheSumOfElements {
+    public static void main(String[] args) {
+        int A[] = {3,9,7,8,12,6,15,5,4,10};
+        int sum = 0;
+        System.out.println("The elements are");
+        for(int x: A){
+            System.out.print(x+" ");
+            sum=sum+x;
+        }
+        System.out.println();
+        System.out.println("The sum of the elements is "+sum);
+    }
+}
