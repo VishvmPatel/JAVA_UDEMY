a2d36e7339098e32a0a84fbb6be635e74b687a01
Vishvam <vishusp77@gmail.com>
2024-11-29 22:07:29 +0530

    update

---
+++ 17.Arrays/src/Practice/Example2.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int B[] = {2,4,8,6,10};
+        for(int i=0;i<B.length;i++){
+            System.out.println(B[i]);
+        }
+    }
+}
