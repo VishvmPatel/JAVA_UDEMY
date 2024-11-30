35f4a579a146910fb2501a7eeb50314eef294dfc
Vishvam <vishusp77@gmail.com>
2024-12-01 00:00:09 +0530

    update

---
+++ 17.Arrays/src/StudentChallenge3/IncreasingLengthOfArray.java
@@ -0,0 +1,14 @@
+package StudentChallenge3;
+
+public class IncreasingLengthOfArray {
+    public static void main(String[] args) {
+        int A[]={8,6,10,9,2};
+        System.out.println("Length of A="+A.length);
+        int B[]=new int[10];
+        for(int i=0;i<A.length;i++) {
+            B[i]=A[i];
+        }
+        A=B;
+        System.out.println("Length of A="+A.length);
+    }
+}
