15c3fb0ed1a41c547572c1f44552cdd346e0bd53
Vishvam <vishusp77@gmail.com>
2024-12-02 17:35:49 +0530

    update

---
+++ 17.Arrays/src/Arrays2D/Example1.java
@@ -0,0 +1,16 @@
+package Arrays2D;
+
+public class Example1 {
+    public static void main(String args[])
+    {
+        int A[][] = new int[3][4];
+        for(int i=0;i<A.length;i++)
+        {
+            for(int j=0;j<A[0].length;j++)
+            {
+                System.out.print(A[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
