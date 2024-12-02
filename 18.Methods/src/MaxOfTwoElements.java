34f3e80376f429f4866ae586676c71c08f1baec6
Vishvam <vishusp77@gmail.com>
2024-12-03 00:01:31 +0530

    update

--- 18.Methods/src/MaxOfTwoElements.java
+++ 18.Methods/src/MaxOfTwoElements.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-
 public class MaxOfTwoElements {
-    static int max (int x , int y){
-        if (x>y){
+    static int max (int x , int y){ //CREATION OF A METHOD
+        if (x>y)
             return x;
-        }
         else
             return y;
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the 2 Numbers:");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = max(a,b);
-        System.out.println("The maximum from the 2 is :"+ c);
+        System.out.println("The maximum from the 2 is: "+ c);
     }
 }
