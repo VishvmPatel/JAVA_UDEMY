1fde424560fd4e701b57418ed0974bcd2b39e0ce
Vishvam <vishusp77@gmail.com>
2024-12-02 23:36:03 +0530

    update

---
+++ 17.Arrays/src/StudentChallenge4/SortingArrayOfStrings.java
@@ -0,0 +1,10 @@
+package StudentChallenge4;
+
+public class SortingArrayOfStrings {
+    public static void main(String[] args) {
+        String arr[]={"java","python","pascal","smalltalk","ada","basic"};
+        java.util.Arrays.sort(arr);
+        for(String x:arr)
+            System.out.println(x);
+    }
+}
