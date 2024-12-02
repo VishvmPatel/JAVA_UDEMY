2c381e8134a8c60e16336bdf39a59227a1d347a8
Vishvam <vishusp77@gmail.com>
2024-12-02 17:38:40 +0530

    update

---
+++ 17.Arrays/src/Arrays2D/Example2.java
@@ -0,0 +1,15 @@
+package Arrays2D;
+
+//SIMILAR BASIC CODE FOR 2D ARRAYS BUT WITH THE HELP OF FOR-EACH LOOP
+
+public class Example2 {
+    public static void main(String[] args) {
+        int A[][] = new int[3][4];
+        for(int[] x:A){
+            for (int y:x){
+                System.out.print(y);
+            }
+            System.out.println();
+        }
+    }
+}
