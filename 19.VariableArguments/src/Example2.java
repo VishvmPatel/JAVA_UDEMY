12a4940c4749ff4d964b09f1b18a0916f38ee585
Vishvam <vishusp77@gmail.com>
2024-12-05 17:14:45 +0530

    update

--- 19.VariableArguments/src/Example2.java
+++ 19.VariableArguments/src/Example2.java
@@ -1,5 +1,10 @@
 public class Example2 {
+    static void showList(String ...S){
+        for(int i=0;i<S.length;i++){
+            System.out.println(i+1+"."+S[i]);
+        }
+    }
     public static void main(String[] args) {
-
+        showList("John","Smith","Ajay","Ankur");
     }
 }
