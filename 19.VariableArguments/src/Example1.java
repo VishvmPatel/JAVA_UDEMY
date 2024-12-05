7383be4ba1f173a49fb190bc247fdc4e79fc79dc
Vishvam <vishusp77@gmail.com>
2024-12-05 17:07:53 +0530

    update

--- 19.VariableArguments/src/Example1.java
+++ 19.VariableArguments/src/Example1.java
@@ -1,3 +1,14 @@
 public class Example1 {
-
+    static void show(int ...A){
+        for(int x : A){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        show();
+        show(10,20,30);
+        show(10,20,30,40,50,60,70,80,90,100);
+        show(new int[]{3,5,7,9,11,13,15}); //CREATED AN ARRAY AND PASSED
+    }
 }
