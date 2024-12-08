a7732f3ba608a69d17b82daf32e7e9559c77509b
Vishvam <vishusp77@gmail.com>
2024-12-08 16:03:11 +0530

    update

---
+++ 21.Recursion/src/Example1.java
@@ -0,0 +1,11 @@
+public class Example1 {
+    static void fun( int n ){
+        if( n > 0){
+            System.out.println(n);
+            fun(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        fun(3);
+    }
+}
