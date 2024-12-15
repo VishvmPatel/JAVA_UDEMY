b37969a5dd2bcf003c2f5bfcd9749b0250634b9a
Vishvam <vishusp77@gmail.com>
2024-12-15 23:36:12 +0530

    update

--- 23.Inheritance/src/MethodOverriding/OverridingExample3.java
+++ 23.Inheritance/src/MethodOverriding/OverridingExample3.java
@@ -1,4 +1,33 @@
 package MethodOverriding;
 
+class Car{
+    public void start(){
+        System.out.println("Car Started");
+    }
+    public void accelerate(){
+        System.out.println("Car is Accelerated");
+    }
+    public void changeGear(){
+        System.out.println("Car Gear Changed");
+    }
+}
+class LuxaryCar extends Car{
+    @Override
+    public void changeGear(){
+        System.out.println("Automatic Gear");
+    }
+    public void openRoof(){
+        System.out.println("Sun Roof is Opened");
+    }
+}
 public class OverridingExample3 {
+    public static void main(String[] args) {
+        Car c = new LuxaryCar();
+        c.start();
+        c.accelerate();
+        c.changeGear();
+        LuxaryCar c1 = new LuxaryCar(); //As openRoof method cannot be called with Car as the reference
+        c1.openRoof(); //(DYNAMIC METHOD DISPATCH)
+
+    }
 }
