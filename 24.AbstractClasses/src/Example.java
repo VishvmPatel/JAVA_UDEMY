a4e420b4ede1c5d467464be966cb941aba4bc265
Vishvam <vishusp77@gmail.com>
2024-12-16 18:19:19 +0530

    update

---
+++ 24.AbstractClasses/src/Example.java
@@ -0,0 +1,31 @@
+// Abstract class
+abstract class Super {
+    // Constructor
+    Super() {
+        System.out.println("Super");
+    }
+    // Non-abstract method
+    void meth1() {
+        System.out.println("meth1");
+    }
+    // Abstract method
+    abstract void meth2();
+}
+// Concrete class
+class Sub extends Super {
+    // Implementation of the abstract method
+    void meth2() {
+        System.out.println("meth2");
+    }
+}
+class Example {
+    public static void main(String[] args) {
+        // Reference of abstract class is allowed
+        Super s1;
+        // Creating an object of the concrete class
+        Sub s2 = new Sub();
+        // Calling methods
+        s2.meth1();
+        s2.meth2();
+    }
+}
