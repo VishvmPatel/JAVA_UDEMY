6adcb85e7e0efdad59114195dbdfb13413e892a3
Vishvam <vishusp77@gmail.com>
2024-12-23 00:05:11 +0530

    update

--- 28.Packages/src/Demo3.java
+++ 28.Packages/src/Demo3.java
@@ -1,6 +1,6 @@
 package MyPackage;
 // Class Demo3 extending Demo1
-public class Demo3 extends Demo1 {
+public class Demo3 extends MyPackage.Demo1 {
     public void show() {
         // 'a' is package-private and not directly accessible here since it's in another file
         // 'b' and 'c' are accessible because 'b' is public and 'c' is protected
