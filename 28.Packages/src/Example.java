58ed6d04270c1660c7313b440bc7117c7aa6073f
Vishvam <vishusp77@gmail.com>
2024-12-22 23:50:32 +0530

    update

--- 28.Packages/src/Example.java
+++ 28.Packages/src/Example.java
@@ -1,6 +1,10 @@
+import mypack1.Demo;
+import mypack1.Demo2;
 public class Example {
     public static void main(String[] args) {
         Demo d1 = new Demo();
         d1.display();
+        Demo2 d2 = new Demo2();
+        d2.display2();
     }
 }
