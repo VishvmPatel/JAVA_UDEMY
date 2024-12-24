a5e8cd615f1dc2acb75f0e6d7faea3c4a96eccf9
Vishvam <vishusp77@gmail.com>
2024-12-24 17:32:32 +0530

    update

---
+++ 29.ExceptionHandling/src/CompilationErrorExample.java
@@ -0,0 +1,7 @@
+public class CompilationErrorExample {
+    public static void main(String[] args) {
+        /* System.out.println(undeclaredVariable); // Compilation Error: undeclaredVariable not defined */
+        int x = 10;
+        System.out.println(x);
+    }
+}
