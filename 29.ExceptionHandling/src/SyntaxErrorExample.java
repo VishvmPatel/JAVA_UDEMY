53bc26f923282a76751ecc2f1a83f829be3bbfd4
Vishvam <vishusp77@gmail.com>
2024-12-24 16:08:11 +0530

    update

---
+++ 29.ExceptionHandling/src/SyntaxErrorExample.java
@@ -0,0 +1,6 @@
+public class SyntaxErrorExample {
+    public static void main(String[] args) {
+        /* System.out.println("Hello, World!") // Missing semicolon */
+        System.out.println("Hello, World!");
+    }
+}
