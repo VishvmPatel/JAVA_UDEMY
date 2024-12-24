889d92b71a23933779224454f8c054e955781ea6
Vishvam <vishusp77@gmail.com>
2024-12-24 22:14:24 +0530

    update

--- 29.ExceptionHandling/src/ExceptionHandling/ArithmeticExceptionHandling.java
+++ 29.ExceptionHandling/src/ExceptionHandling/ArithmeticExceptionHandling.java
@@ -4,6 +4,7 @@ public class ArithmeticExceptionHandling {
     public static void main(String[] args) {
         try{
             int result = 10/0 ; // This will throw ArithmeticException
+            System.out.println(result);
         }
         catch (ArithmeticException e){
             System.out.println("Exception caught: Division by zero is not allowed.");
