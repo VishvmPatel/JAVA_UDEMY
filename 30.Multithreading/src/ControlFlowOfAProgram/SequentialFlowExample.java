e2c3fd6f722f827741fc0d302152b5de9a1a1f77
Vishvam <vishusp77@gmail.com>
2024-12-28 11:51:14 +0530

    update

---
+++ 30.Multithreading/src/ControlFlowOfAProgram/SequentialFlowExample.java
@@ -0,0 +1,9 @@
+package ControlFlowOfAProgram;
+
+public class SequentialFlowExample {
+    public static void main(String[] args) {
+        System.out.println("Step 1: Start");
+        System.out.println("Step 2: Process");
+        System.out.println("Step 3: End");
+    }
+}
