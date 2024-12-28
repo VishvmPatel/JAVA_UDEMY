96ddb1836a910883993d110697402e0635d40ceb
Vishvam <vishusp77@gmail.com>
2024-12-28 14:33:24 +0530

    update

---
+++ 30.Multithreading/src/ControlFlowOfAProgram/ConditionalFlowExample1.java
@@ -0,0 +1,12 @@
+package ControlFlowOfAProgram;
+
+public class ConditionalFlowExample1 {
+    public static void main(String[] args) {
+        int number = 5;
+        if(number > 0){
+            System.out.println("Positive Number");
+        }else{
+            System.out.println("Negative Number");
+        }
+    }
+}
