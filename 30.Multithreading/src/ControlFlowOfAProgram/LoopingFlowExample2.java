0b76dfe832fe71bba996c77d17f52a2315cf8a48
Vishvam <vishusp77@gmail.com>
2024-12-28 15:42:22 +0530

    update

---
+++ 30.Multithreading/src/ControlFlowOfAProgram/LoopingFlowExample2.java
@@ -0,0 +1,13 @@
+package ControlFlowOfAProgram;
+
+// WHILE LOOP
+
+public class LoopingFlowExample2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=5){
+            System.out.println("Count: "+i);
+            i++;
+        }
+    }
+}
