381c47a803b8d837aa1515dcc3f4497cdc39e542
Vishvam <vishusp77@gmail.com>
2025-01-15 13:52:54 +0530

    update

---
+++ 31.JavaLangPackage/src/StringClassExample.java
@@ -0,0 +1,8 @@
+public class StringClassExample {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = s1.concat(" World");
+        System.out.println(s1); // Output: Hello (unchanged)
+        System.out.println(s2); // Output: Hello World
+    }
+}
