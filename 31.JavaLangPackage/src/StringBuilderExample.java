b3db82608819bb6e4af3b05c9374522fa123b287
Vishvam <vishusp77@gmail.com>
2025-01-15 13:52:46 +0530

    update

---
+++ 31.JavaLangPackage/src/StringBuilderExample.java
@@ -0,0 +1,7 @@
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println(sb); // Output: Hello World
+    }
+}
